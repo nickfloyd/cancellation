e0f8b39e45cc509d6817d713e581237707fb93fb
Nick Floyd <nicholas.floyd.info@gmail.com>
2022-03-02 15:06:13 -0600

    adds event

--- cancellation.gemspec
+++ cancellation.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'cancellation'
-  s.version     = '0.0.4'
+  s.version     = '0.0.5'
   s.summary     = "Cancellation!"
   s.description = "Cancellation in Ruby"
   s.authors     = ["Nick Floyd"]
---
+++ lib/event.rb
@@ -0,0 +1,31 @@
+# A simple waitable one-time event.
+# TODO: does this exist in a library?
+class Event
+  def initialize
+    @mu = Mutex.new
+    @done = false
+    @cv = ConditionVariable.new # for "done" condition
+  end
+
+  # broadcast causes the event to occur.
+  def broadcast
+    @mu.synchronize {
+      @done = true
+      @cv.broadcast
+    }
+  end
+
+  # wait puts the current thread to sleep until the event occurs.
+  def wait
+    @mu.synchronize {
+      while !@done
+        @cv.wait(@mu)
+      end
+    }
+  end
+
+  # fired? reports whether the event has occurred.
+  def fired?
+    @mu.synchronize { @done }
+  end
+end
