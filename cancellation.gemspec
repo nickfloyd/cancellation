ea626a24f7b4ba69e0d21a6a7495eaeb5282775a
Nick Floyd <nicholas.floyd.info@gmail.com>
2022-03-02 13:49:31 -0600

    adds basic gem

---
+++ cancellation.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'cancellation'
+  s.version     = '0.0.0'
+  s.summary     = "Cancellation!"
+  s.description = "Cancellation in Ruby"
+  s.authors     = ["Nick Floyd"]
+  s.email       = 'nick@floyd.com'
+  s.files       = ["lib/cancellation.rb"]
+  s.homepage    =
+    'https://rubygems.org/gems/cancellation'
+  s.license       = 'MIT'
+end
---
+++ lib/cancellation.rb
@@ -0,0 +1,30 @@
+class Cancellation
+  def self.hi
+    puts "Hello world!"
+  end
+
+  # with_cancel evaluates 'block', and calls 'abort' in another
+  # thread if the thread-local cancel event occurs before or during
+  # (or possibly even shortly after) the call is complete.
+  # The abort and block procedures must be safe to call concurrently.
+  #
+  # Use this around any potentially slow operation that
+  # supports asynchronous cancellation.
+  def with_cancel(abort, &block)
+    mu = Mutex.new # TODO: does Ruby have an AtomicBoolean?
+    done = false
+
+    cancel = Thread.current[:cancel] # TODO crash if not present
+    
+    # The watchdog thread may be killed at any moment.
+    watchdog = Thread.new do
+      cancel.wait
+      abort.call unless mu.synchronize { done }
+    end
+    block.call
+  ensure
+    mu.synchronize { done = true }
+    watchdog.kill
+    watchdog.join
+  end
+end
